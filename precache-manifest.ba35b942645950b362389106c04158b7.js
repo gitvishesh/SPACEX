d30a174bf8e2a1a295a22e7cd5f25261181802ac
vishesh malik <vishmalik25@gmail.com>
2020-08-11 10:37:10 +0530

    Updates

---
+++ precache-manifest.ba35b942645950b362389106c04158b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3607b80d988ea50a1c0a0ec0d601e69",
+    "url": "/SPACEX/index.html"
+  },
+  {
+    "revision": "450fc10d56230ebe9fbf",
+    "url": "/SPACEX/static/css/main.f8685295.chunk.css"
+  },
+  {
+    "revision": "d7882100e08638b80386",
+    "url": "/SPACEX/static/js/2.b34f95b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SPACEX/static/js/2.b34f95b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450fc10d56230ebe9fbf",
+    "url": "/SPACEX/static/js/main.4e522484.chunk.js"
+  },
+  {
+    "revision": "b49948cf8dc07b319dbf",
+    "url": "/SPACEX/static/js/runtime-main.5693d295.js"
+  }
+]);
